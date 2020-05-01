c0427b2c961a0da9ab6cdfa0ac8fbabc32e4b3a9
firework4lj <firework4lj@gmail.com>
2020-05-01 15:39:19 -0700

    Updated Arduino code

--- testarduino/testarduino.ino
+++ testarduino/testarduino.ino
@@ -1,9 +1,11 @@
 const byte numChars = 64;
 char receivedChars[numChars];
+
 boolean newData = false;
 const int buttonPin = 2;
 byte ledPin = 13;   // the onboard LED
-int buttonState = 0;
+byte vibrationpin = 12;
+int buttonState = 1;
 
 //===============
 
@@ -12,7 +14,9 @@ void setup() {
 
     pinMode(buttonPin, INPUT);
     pinMode(ledPin, OUTPUT);
+    pinMode(vibrationpin, OUTPUT);
     digitalWrite(ledPin, HIGH);
+    digitalWrite(vibrationpin, LOW);
     delay(200);
     digitalWrite(ledPin, LOW);
    
@@ -80,18 +84,35 @@ void start_running(){
 
 
 void send_packet(){
-  char packet[8];
-  /*
+ //Button 1/0
+ //signed double for accellerometer
+ //0-1024 for each pressure sensor
+ //
+ //          PS0  PS1 B
+ // PACKET: XXXX XXXX X
+  char button;
+  if(digitalRead(11)){
+    button='0';
+  }
+  else{
+    button='1';
+  }
+  int buttonRead;
+//  buttonRead=digitalRead(2);
+ // char button = buttonRead;
+  char packet[10];
   packet[0]='A';
-  packet[1]='A';
-  packet[2]='A';
-  packet[3]='A';
-  packet[4]='A';
-  packet[5]='A';
-  packet[6]='A';
-  packet[7]='\n';
+  packet[1]='B';
+  packet[2]='C';
+  packet[3]='D';
+  packet[4]='E';
+  packet[5]='F';
+  packet[6]='G';
+  packet[7]='H';
+  packet[8]=button;
+  packet[9]='\n';
+  delay(50);
   Serial.print(packet);
-  */
 }
 
 //===============
@@ -104,10 +125,13 @@ void check_for_request(){
     char endMarker = '>';
     char rc;
     int l=0;
+    newData = false;
+   
    while (Serial.available() > 0 && newData == false) {
         rc = Serial.read();
         l++;
-            if (rc != '.') {
+  //      if (recvInProgress == true) {
+            if (rc != '\n') {
                 receivedChars[ndx] = rc;
                 ndx++;
                 if (ndx >= numChars) {
@@ -120,21 +144,27 @@ void check_for_request(){
                 ndx = 0;
                 newData = true;
             }
-   }
-   newData=false;
-   Serial.print(receivedChars[0]);
-         if(rc == "L0"){
+      //  }
+   //     else{
+     //      
+       //     recvInProgress = true;
+        //}
+       
+    }
+    if(receivedChars[0]=='L'){
+         if(receivedChars[1]=='0'){
            digitalWrite(ledPin, LOW);
          }
-         if(rc == "L1"){
+         if(receivedChars[1]=='1'){
            digitalWrite(ledPin, HIGH);
          }
+    }
     if(receivedChars[0]=='V'){
-         if(receivedChars[1]=='0'){
-           //Vibration OFF
-         }
          if(receivedChars[1]=='1'){
-           //Vibration ON
+           digitalWrite(vibrationpin,HIGH);
+         }
+         if(receivedChars[1]=='0'){
+           digitalWrite(vibrationpin,LOW);
          }
     }
     if(receivedChars[0]=='S'){
@@ -150,6 +180,37 @@ void check_for_request(){
 //===============
 
 //===============
+void receive_start_signal() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
 
 void reply_with_results(int results) {
     if (newData == true) {
